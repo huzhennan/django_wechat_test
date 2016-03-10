81927ec194ff4409bc050ff4319db3b5f81660a5
huzhennan <huzhennan@kezaihui.com>
2016-03-10 13:32:20 +0800

    update doc

--- chat/core.py
+++ chat/core.py
@@ -9,8 +9,19 @@ def generate_url(base_url, params):
     return base_url + "?" + urllib.urlencode(params)
 
 
-class WeAuthMixin(object):
+class WebAuthMixin(object):
+    """
+    http://mp.weixin.qq.com/wiki/4/9ac2e7b1f1d22e9e57260f6553822520.html
+    """
     def generate_auth_url(self, redirect_uri, scope='snsapi_base', state='test'):
+        """
+        生成网页制授权页面(对应第一步:用户同意授权，获取code)
+        :param redirect_uri: 授权后重定向的回调链接地址，请使用urlencode对链接进行处理
+        :param scope: 应用授权作用域，snsapi_base （不弹出授权页面，直接跳转，只能获取用户openid）,
+        snsapi_userinfo （弹出授权页面，可通过openid拿到昵称、性别、所在地。
+        :param state: 重定向后会带上state参数，开发者可以填写a-zA-Z0-9的参数值，最多128字节
+        :return:
+        """
         params = [
             ('appid', self.app_id()),
             ('redirect_uri', redirect_uri),
@@ -21,6 +32,11 @@ class WeAuthMixin(object):
         return generate_url('https://open.weixin.qq.com/connect/oauth2/authorize', params)
 
     def get_web_token(self, code):
+        """
+        通过code换取网页授权access_token(对应第二步)
+        :param code:
+        :return:
+        """
         url = u"https://api.weixin.qq.com/sns/oauth2/access_token"
         params = (
             ('appid', self.app_id()),
@@ -33,6 +49,11 @@ class WeAuthMixin(object):
         return r.json()
 
     def get_open_id(self, code):
+        """
+        获取open_id, 从access_token信息中
+        :param code:
+        :return:
+        """
         return self.get_web_token(code)[u'openid']
 
     def app_id(self):
@@ -44,6 +65,11 @@ class WeAuthMixin(object):
 
 class WeBaseMixin(object):
     def create_menu(self, menu_data):
+        """
+        自定义菜单创建接口
+        :param menu_data: 菜单json数据
+        :return:
+        """
         conf = WechatConf(
             token=self.app_token(),
             appid=self.app_id(),
@@ -63,7 +89,7 @@ class WeBaseMixin(object):
         return NotImplementedError()
 
 
-class WeClient(WeAuthMixin, WeBaseMixin):
+class WebClient(WebAuthMixin, WeBaseMixin):
     def __init__(self, app_id, app_secret, app_token):
         self.__app_id = app_id
         self.__app_secret = app_secret
--- chat/handles.py
+++ chat/handles.py
@@ -1,7 +1,7 @@
 # encoding=utf-8
 from __future__ import absolute_import
 
-from .core import WeClient
+from .core import WebClient
 
 TOKEN = u"ZaiHuiWanSui2015"
 APP_ID = u"wxd1ac16e44122c49a"
@@ -9,7 +9,7 @@ APP_SECRET = u"d3ddce902a9d3c1f2071eb25f479df33"
 
 
 def generate_test_menu(redirect_url='http://www.baidu.com'):
-    client = WeClient(APP_ID, APP_SECRET, APP_SECRET)
+    client = WebClient(APP_ID, APP_SECRET, APP_SECRET)
 
     menu = {
         'button': [
@@ -31,10 +31,10 @@ def generate_test_menu(redirect_url='http://www.baidu.com'):
         ]
     }
 
-    client = WeClient(APP_ID, APP_SECRET, APP_SECRET)
+    client = WebClient(APP_ID, APP_SECRET, APP_SECRET)
     client.create_menu(menu)
 
 
 def get_open_id(code):
-    client = WeClient(APP_ID, APP_SECRET, APP_SECRET)
+    client = WebClient(APP_ID, APP_SECRET, APP_SECRET)
     return client.get_open_id(code)
