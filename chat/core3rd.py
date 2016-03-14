8725cfebb6515d17e14a701612ff9d7e25064892
huzhennan <huzhennan@kezaihui.com>
2016-03-14 11:30:28 +0800

    update 3re wechat code

--- chat/core3rd.py
+++ chat/core3rd.py
@@ -1,8 +1,10 @@
 # encoding=utf-8
 import logging
 
+import redis
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from wechat_sdk import WechatConf, WechatBasic
 from wechat_sdk.lib.crypto import BasicCrypto
 
 logger = logging.getLogger(__name__)
@@ -10,10 +12,32 @@ logger = logging.getLogger(__name__)
 TOKEN = u'ZaiHuiNiuBi20151102'
 SYMMETRIC_KEY = u'yBUilBquwak6qAzuL1U04JrFSjwDjukjDoFjPRuLCU2'
 APP_ID = u'wx67c082d3d5c5c355'
+APP_SECRET = u'7678827bbe43445e4fb6631cd96e02dc'
+
+store = redis.StrictRedis(host='localhost', port=6379)
+
+
+def client3rd():
+    c = We3rdClient(APP_ID, APP_SECRET, TOKEN, SYMMETRIC_KEY, store=store)
+    return c
+
+
+class We3rdClient(WechatBasic):
+    def __init__(self, app_id, app_secret, app_token, encoding_aes_key, store, encrypt_mode='safe'):
+        conf = WechatConf(
+            appid=app_id,
+            appsecret=app_secret,
+            token=app_token,
+            encrypt_mode=encrypt_mode,
+            encoding_aes_key=encoding_aes_key
+        )
+        WechatBasic.__init__(self, conf=conf)
+        self.__app_id = app_id
+        self.__app_secret = app_secret
+        self.__store = store
 
 
 class We3rdResponse(object):
-
     @staticmethod
     @csrf_exempt
     def receive_verify_ticket(request):
@@ -21,10 +45,14 @@ class We3rdResponse(object):
         timestamp = request.GET.get('timestamp')
         nonce = request.GET.get('nonce')
 
-        crypto = BasicCrypto(TOKEN, SYMMETRIC_KEY, APP_ID)
-        msg = crypto.decrypt_message(msg=request.body, msg_signature=msg_signature, nonce=nonce, timestamp=timestamp)
+        logger.info("before msg: %r", request.body)
 
+        # crypto = BasicCrypto(TOKEN, SYMMETRIC_KEY, APP_ID)
+        # msg = crypto.decrypt_message(msg=request.body, msg_signature=msg_signature, nonce=nonce, timestamp=timestamp)
 
+        client = client3rd()
+        client.parse_data(data=request.body, msg_signature=msg_signature, timestamp=timestamp, nonce=nonce)
+        msg = client.get_message()
 
         logger.info("msg: %r", msg)
         return HttpResponse(u"success")
