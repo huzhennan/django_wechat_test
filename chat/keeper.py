282c85c7238bf71735519773174456d61793bc76
huzhennan <huzhennan@kezaihui.com>
2016-03-15 10:28:32 +0800

    refact keeper

--- chat/keeper.py
+++ chat/keeper.py
@@ -1,9 +1,10 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-import redis
+import json
 import logging
-import ast
+
+import redis
 
 logger = logging.getLogger(__name__)
 
@@ -11,58 +12,54 @@ logger = logging.getLogger(__name__)
 class Keeper(object):
     EXPIRES_IN = u'expires_in'
 
-    def __init__(self, store, key, gain_func=None, shorten=0):
+    def __init__(self, store, gain_func=None, shorten=0):
         """
         :param store: redis存储
-        :param key: 用来生成key
         :param gain_func: 从网上获取taken的方法
         :param shorten: 缩短`expires_in`大小
         """
         self.__store = store
-        self.__key = key
         self.__gain_func = gain_func
         self.__shorten = shorten
 
-    def get(self):
-        var = self.store.get(self.key)
+    def get(self, key):
+        var = self.store.get(key)
         if var:
-            logger.debug("get %r = %r from store", self.key, var)
-            return ast.literal_eval(var)
+            var = json.loads(var)
+            logger.debug("get %r = %r from store", key, var)
+            return var
         else:
             if callable(self.gain_func):
                 var = self.gain_func()
-                logger.debug("get %r = %r from %r", self.key, var, self.gain_func)
+                logger.debug("get %r = %r from %r", key, var, self.gain_func)
                 expires_in = var.get(Keeper.EXPIRES_IN, None)
                 if expires_in:
                     expires_in = int(expires_in) - self.shorten
 
+                var_json = json.dumps(var)
                 if expires_in:
-                    self.store.setex(self.key, expires_in, var)
+                    self.store.setex(key, expires_in, var_json)
                 else:
-                    self.store.set(self.key, var)
+                    self.store.set(key, var_json)
                 return var
             else:
                 raise RuntimeError("I need get a way to get value")
 
-    def setex(self, name, value, time):
+    def setex(self, key, value, time):
         """
         Set the value of key name to value that expires in time seconds
         """
-        return self.store.setex(name, value, time)
+        return self.store.setex(key, value, time)
 
-    def set(self, name, value):
+    def set(self, key, value):
         """
         Set the value at key name to value
         """
-        return self.set(name, value)
+        return self.set(key, value)
 
     @property
     def store(self):
         return self.__store
-
-    @property
-    def key(self):
-        return self.__key
 
     @property
     def gain_func(self):
@@ -81,8 +78,11 @@ if __name__ == '__main__':
         def gain_func(self):
             return {"key": "this is"}
 
+
     obj = A()
     store = redis.StrictRedis(host='localhost', port=6379)
-    keeper = Keeper(store, "access_token", obj.gain_func)
+    keeper = Keeper(store, obj.gain_func)
 
-    print "%r %r" % ('haha', keeper.get())
+    ret = keeper.get("access_token")
+
+    print "%r %r" % (type(ret), ret)
