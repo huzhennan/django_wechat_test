6f0042916138188326dd90b0c18f6f5da8ca34a7
huzhennan <huzhennan@kezaihui.com>
2016-03-17 10:40:24 +0800

    update test menu date 2

--- chat/handles.py
+++ chat/handles.py
@@ -6,38 +6,38 @@ from chat.utils import generate_auth_url
 
 def generate_test_menu(client, app_id, redirect_url='http://www.baidu.com', component_appid=None):
     client.create_menu({
-                'button':[
+        'button': [
+            {
+                'type': 'click',
+                'name': 'hzn',
+                'key': 'V1001_TODAY_MUSIC'
+            },
+            {
+                'type': 'click',
+                'name': '歌手简介',
+                'key': 'V1001_TODAY_SINGER'
+            },
+            {
+                'name': '菜单',
+                'sub_button': [
                     {
-                        'type': 'click',
-                        'name': '今日歌曲',
-                        'key': 'V1001_TODAY_MUSIC'
+                        'type': 'view',
+                        'name': '搜索',
+                        'url': 'http://www.soso.com/'
+                    },
+                    {
+                        'type': 'view',
+                        'name': '视频',
+                        'url': 'http://v.qq.com/'
                     },
                     {
                         'type': 'click',
-                        'name': '歌手简介',
-                        'key': 'V1001_TODAY_SINGER'
-                    },
-                    {
-                        'name': '菜单',
-                        'sub_button': [
-                            {
-                                'type': 'view',
-                                'name': '搜索',
-                                'url': 'http://www.soso.com/'
-                            },
-                            {
-                                'type': 'view',
-                                'name': '视频',
-                                'url': 'http://v.qq.com/'
-                            },
-                            {
-                                'type': 'click',
-                                'name': '赞一下我们',
-                                'key': 'V1001_GOOD'
-                            }
-                        ]
+                        'name': '赞一下我们',
+                        'key': 'V1001_GOOD'
                     }
-                ]})
+                ]
+            }
+        ]})
 
 
 def get_open_id(client, code):
